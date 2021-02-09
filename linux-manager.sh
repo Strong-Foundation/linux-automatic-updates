0e2e450a6caf9869fb7574f84f888095f39f3caf
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-09 12:44:59 -0500

    Create linux-manager.sh

---
+++ linux-manager.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Require script to be run as root
+function super-user-check() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "You need to run this script as super user."
+    exit
+  fi
+}
+
+# Check for root
+super-user-check
+
+# Detect Operating System
+function dist-check() {
+  if [ -e /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    DISTRO=$ID
+  fi
+}
+
+# Check Operating System
+dist-check
+
+# Pre-Checks system requirements
+function installing-system-requirements() {
+  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+    if [ ! -x "$(command -v curl)" ]; then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+        apt-get update && apt-get install curl -y
+      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+        yum update -y && yum install curl -y
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+        pacman -Syu && pacman -Syu --noconfirm curl
+      elif [ "$DISTRO" == "alpine" ]; then
+        apk update && yum install curl -y
+      elif [ "$DISTRO" == "freebsd" ]; then
+        pkg update && pkg install curl
+      fi
+    fi
+  else
+    echo "Error: $DISTRO not supported."
+    exit
+  fi
+}
+
+# Run the function and check for requirements
+installing-system-requirements
+
+# Pre-Checks
+function start-the-process() {
+  # cat /dev/null > ~/.bash_history && history -c && exit
+  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then    
+    apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install build-essential unattended-upgrades apt-listchanges -y && dpkg-reconfigure unattended-upgrades && apt-get clean -y && apt-get autoremove -y && apt-get install --assume-yes --fix-broken && apt-get install git golang-go -y
+  elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+    yum update -y && yum upgrade -y && yum autoremove -y
+  elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+    pacman -Syu --noconfirm
+  elif [ "$DISTRO" == "alpine" ]; then
+    # 
+  elif [ "$DISTRO" == "freebsd" ]; then
+    #
+  fi
+}
+
+# Run the function and check for requirements
+start-the-process
