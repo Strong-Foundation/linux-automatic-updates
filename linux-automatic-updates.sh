b5500daf4e72c5d171e4c1a311fab65f2a1c9157
Prajwal Koirala <179532304+Strong-Foundation@users.noreply.github.com>
2025-02-28 05:54:31 -0500

    update

--- linux-automatic-updates.sh
+++ linux-automatic-updates.sh
@@ -2,7 +2,7 @@
 # https://github.com/complexorganizations/linux-automatic-updates
 
 # Require script to be run as root
-function super-user-check() {
+function super_user_check() {
   if [ "${EUID}" -ne 0 ]; then
     echo "Error: You need to run this script as administrator."
     exit
@@ -10,10 +10,10 @@ function super-user-check() {
 }
 
 # Check for root
-super-user-check
+super_user_check
 
 # Get the current system information
-function system-information() {
+function system_information() {
   if [ -f /etc/os-release ]; then
     # shellcheck source=/dev/null
     source /etc/os-release
@@ -22,37 +22,55 @@ function system-information() {
 }
 
 # Get the current system information
-system-information
+system_information
 
-# Pre-Checks system requirements
-function installing-system-requirements() {
-  case ${CURRENT_DISTRO} in
-    "ubuntu" | "debian" | "raspbian" | "pop" | "kali" | "linuxmint" | "neon" | "fedora" | "centos" | "rhel" | "almalinux" | "rocky" | "arch" | "archarm" | "manjaro" | "alpine" | "freebsd" | "ol") ;;
-  *)
+# Validate if the system is supported
+function check_supported_distro() {
+  # Supported distributions
+  SUPPORTED_DISTROS=("ubuntu" "debian" "raspbian" "pop" "kali" "linuxmint" "neon"
+    "fedora" "centos" "rhel" "almalinux" "rocky" "arch" "archarm"
+    "manjaro" "alpine" "freebsd" "ol" "opensuse" "sles")
+  # Check if the current distribution is supported
+  if [[ ! " ${SUPPORTED_DISTROS[*]} " =~ " ${CURRENT_DISTRO} " ]]; then
+    # If the distribution is not supported, exit the script
     echo "Error: ${CURRENT_DISTRO} is not supported."
-    exit
-    ;;
-  esac
+    # Exit the script
+    exit 1
+  fi
 }
 
-# Run the function and check for requirements
-installing-system-requirements
+# Run the check for supported distributions
+check_supported_distro
 
 # Update linux completely.
-function update-linux-completely() {
+function update_linux_completely() {
   if { [ "${CURRENT_DISTRO}" == "ubuntu" ] || [ "${CURRENT_DISTRO}" == "debian" ] || [ "${CURRENT_DISTRO}" == "raspbian" ] || [ "${CURRENT_DISTRO}" == "pop" ] || [ "${CURRENT_DISTRO}" == "kali" ] || [ "${CURRENT_DISTRO}" == "linuxmint" ] || [ "${CURRENT_DISTRO}" == "neon" ]; }; then
     apt-get update
-    apt-get install unattended-upgrades --yes
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get clean -y
+    apt-get autoremove -y
+    apt-get autoclean -y
+    apt-get install unattended-upgrades -y
   elif { [ "${CURRENT_DISTRO}" == "fedora" ] || [ "${CURRENT_DISTRO}" == "centos" ] || [ "${CURRENT_DISTRO}" == "rhel" ] || [ "${CURRENT_DISTRO}" == "almalinux" ] || [ "${CURRENT_DISTRO}" == "rocky" ] || [ "${CURRENT_DISTRO}" == "ol" ]; }; then
-    yum check-update
+    dnf update -y
+    dnf upgrade -y
+    dnf clean all
+    dnf autoremove -y
   elif { [ "${CURRENT_DISTRO}" == "arch" ] || [ "${CURRENT_DISTRO}" == "archarm" ] || [ "${CURRENT_DISTRO}" == "manjaro" ]; }; then
-    pacman -Sy
+    pacman -Syu --noconfirm
+    pacman -Sc --noconfirm
   elif [ "${CURRENT_DISTRO}" == "alpine" ]; then
     apk update
+    apk upgrade --available
   elif [ "${CURRENT_DISTRO}" == "freebsd" ]; then
     pkg update
+    pkg upgrade -y
+  elif { [ "${CURRENT_DISTRO}" == "opensuse" ] || [ "${CURRENT_DISTRO}" == "sles" ]; }; then
+    zypper refresh
+    zypper update -y
   fi
 }
 
 # Completely update linux
-update-linux-completely
+update_linux_completely
